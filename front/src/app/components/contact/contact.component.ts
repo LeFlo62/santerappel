9241c2e5918220f1fc9bbaabb44df2e5a68655a9
Nathan WEBER <nathan.weber@eleve.isep.fr>
2023-06-02 01:01:42 +0200

    added 10 characters limits in form contact

--- front/src/app/components/contact/contact.component.ts
+++ front/src/app/components/contact/contact.component.ts
@@ -15,6 +15,8 @@ export class ContactComponent {
   onSubmit() {
     if (!this.firstname || !this.lastname || !this.phone || !this.subject || !this.content) {
       alert('Veilliez remplir tous les champs');
+    } else if (this.content.length > 100) {
+      alert('Le contenu ne peut pas dépasser 100 caractères');
     } else {
       const mailtoLink = `mailto:someone@example.com?subject=${this.subject}&body=Name: ${this.firstname} ${this.lastname}%0D%0APhone: ${this.phone}%0D%0AMessage: ${this.content}`;
       window.location.href = mailtoLink;
