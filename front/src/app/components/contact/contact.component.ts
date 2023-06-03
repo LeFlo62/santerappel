e04f951f4224819f5480cf42fe47fba90fb4e1c5
Nathan WEBER <nathan.weber@eleve.isep.fr>
2023-06-03 03:52:21 +0200

    contact form improvements

--- front/src/app/components/contact/contact.component.ts
+++ front/src/app/components/contact/contact.component.ts
@@ -1,26 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.scss']
 })
-export class ContactComponent {
-  firstname = '';
-  lastname = '';
-  phone = '';
-  subject = '';
-  content = '';
+export class ContactComponent implements OnInit {
+  contactForm!: FormGroup;
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit() {
+    this.contactForm = this.fb.group({
+      firstname: ['', [Validators.pattern('^[a-zA-Z ]*$')]],
+      lastname: ['', [Validators.pattern('^[a-zA-Z ]*$')]],
+      phone: ['', [Validators.required, Validators.pattern('^0[6-7][0-9]{8}$')]],
+      content: ['', [Validators.required, Validators.maxLength(100)]],
+      subject: ['', [Validators.required, Validators.maxLength(300)]]
+    });
+  }
 
   onSubmit() {
-    if (!this.firstname || !this.lastname || !this.phone || !this.subject || !this.content) {
-      alert('Veilliez remplir tous les champs');
-    } else if (this.content.length > 100) {
-      alert('Le contenu ne peut pas dépasser 100 caractères');
+    if (
+      (this.contactForm.controls['firstname'].invalid && this.contactForm.controls['firstname'].dirty) ||
+      (this.contactForm.controls['lastname'].invalid && this.contactForm.controls['lastname'].dirty) ||
+      (this.contactForm.controls['phone'].invalid && this.contactForm.controls['phone'].dirty) ||
+      (this.contactForm.controls['content'].invalid && this.contactForm.controls['content'].dirty) ||
+      (this.contactForm.controls['subject'].invalid && this.contactForm.controls['subject'].dirty)
+    ) {
+      alert('Veillez remplir tous les champs correctement');
     } else {
-      const mailtoLink = `mailto:someone@example.com?subject=${this.subject}&body=Name: ${this.firstname} ${this.lastname}%0D%0APhone: ${this.phone}%0D%0AMessage: ${this.content}`;
+      const mailtoLink = `mailto:someone@example.com?subject=${this.contactForm.value.subject}&body=Name: ${this.contactForm.value.firstname} ${this.contactForm.value.lastname}%0D%0APhone: ${this.contactForm.value.phone}%0D%0AMessage: ${this.contactForm.value.content}`;
       window.location.href = mailtoLink;
     }
   }
-
 }
